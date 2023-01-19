f55213e1b591e55221f39d16aced7fc8b44819cc
BrianTahiel <briang.2012@hotmail.com>
2023-01-19 15:14:42 -0300

    minor fixes

--- script.js
+++ script.js
@@ -1,97 +1,136 @@
-String.prototype.replaceAt=function(index, character){
-    return this.substring(0, index) + character + this.substring(index + character.length);}
+String.prototype.replaceAt = function (index, character) {
+  return (
+    this.substring(0, index) +
+    character +
+    this.substring(index + character.length)
+  );
+};
 
-function addWord(){
-    let newW = document.querySelector("#btn-addWord").value;
-    words.push(newW)
-    console.log(words)
+let gameOverBtn = document.querySelector(".gameOver").disabled = true;
+document.querySelector(".container-keyboard").style.visibility = "hidden";
+document.querySelector(".container-game").style.visibility = "visible";
+let btn_letters = document.querySelectorAll(".keyboard button");
+
+function showMessage(){
+    document.querySelector(".message").style.visibility = "visible";
 }
 
+for (let i = 0; i < btn_letters.length; i++) {
+  btn_letters[i].addEventListener("click", function () {
+    btn_letters[i].disabled = false;
+  });
+}
+
+function addWord() {
+  let newW = document.querySelector("#btn-addWord").value.toUpperCase();
+  if(newW == ""){
+    alert("Please enter a new word")
+  }
+  else if(!words.includes(newW)){
+    words.push(newW);
+    alert("The word was added correctly!")
+    console.log(words);
+  }
+  else{
+    alert("Repeated word")
+  }
+}
 let randomWord = "";
 let words = ["JAVA", "JAVASCRIPT", "PYTHON"];
 const btn = document.getElementById("startGame");
-btn.addEventListener('click', startGame);
+const btnEndGame = document.getElementById("");
+btn.addEventListener("click", startGame);
 let countFail = 0;
 let countHit = 0;
-
-//It disables each letter when used.
-
-let btn_letters = document.querySelectorAll(".keyboard button");
-
 for (let i = 0; i < btn_letters.length; i++) {
-    btn_letters[i].addEventListener("click", function() {
-        btn_letters[i].disabled = true;
+    btn_letters[i].addEventListener("click", function () {
+      btn_letters[i].disabled = true;
     });
+  }
+function startGame() {
+   btn.disabled = true;
+  usedLetters = [];
+  countFail = 0;
+  countHit = 0;  
+  document.querySelector(".message").style.visibility = "hidden";
+  document.querySelector(".underscore").style.visibility = "visible";
+  document.querySelector(".gameOver").style.visibility = "visible";
+  document.querySelector(".addWord-container").style.visibility = "hidden";
+  gameOverBtn = document.querySelector(".gameOver").disabled = false;
+  document.getElementById("imagen").style.visibility = "visible";
+  document.querySelector(".usedLetters").style.visibility = "visible";
+  document.querySelector(".usedLetters").innerHTML = "";
+  document.querySelector(".container-keyboard").style.visibility = "visible";
+  document.querySelector(".container-game").style.visibility = "visible";
+  document.getElementById("imagen").src = `./images/img00.png`;
+  document.querySelector(".underscore").innerHTML = "";
+  const countWords = words.length;
+  const randomValue = Math.floor(Math.random() * countWords);
+  randomWord = words[randomValue];
+  underscore = randomWord.replace(/./g, "_ ");
+  document.querySelector(".underscore").innerHTML = underscore;
 }
 
-function clickOnLetter(e){
-    let btn = e.target;
-    const letter = btn.innerHTML
-    let ifGuessed = false;
+function clickOnLetter(e) {
+  let btn = e.target;
+  const letter = btn.innerHTML;
+  let ifGuessed = false;
 
-    for(let i in randomWord){
-        document.querySelector(".underscore").innerHTML = underscore;
-        if(letter == randomWord[i]){
-            underscore = underscore.replaceAt(i*2, letter); 
-            countHit++
-            ifGuessed = true
-        }
+  for (let i in randomWord) {
+    document.querySelector(".underscore").innerHTML = underscore;
+    if (letter == randomWord[i]) {
+      underscore = underscore.replaceAt(i * 2, letter);
+      countHit++;
+      ifGuessed = true;
     }
-    if(ifGuessed == false){
-        countFail++
-        document.getElementById("imagen").src=`./images/img0${countFail}.png`;   
-    }
-    if(countFail == 7){
-        document.querySelector(".underscore").innerHTML = "Perdiste, la palabra era " + randomWord;
-        endGame()
-
-    }
-    else if(countHit == randomWord.length){
-        document.querySelector(".underscore").innerHTML = "Ganaste";
-        endGame()
-    }
-    usedLetters.push(letter)
-    let onlyOnes = [...new Set(usedLetters)];
-    document.querySelector(".usedLetters").innerHTML = onlyOnes
-    // console.log(onlyOnes)
+  }
+  if (ifGuessed == false) {
+    countFail++;
+    document.getElementById("imagen").src = `./images/img0${countFail}.png`;
+  }
+  if (countFail == 7) {
+    document.querySelector(".message").innerHTML =
+      "You failed, the word was " + randomWord;
+    showMessage();
+    endGame();
+  } else if (countHit == randomWord.length) {
+    document.querySelector(".message").innerHTML = "Spot on!";
+    showMessage();
+    endGame();
+  }
+  usedLetters.push(letter);
+  let onlyOnes = [...new Set(usedLetters)];
+  document.querySelector(".usedLetters").innerHTML = onlyOnes;
 }
-
-function startGame(){
-    btn.disabled = true;
-    document.querySelector(".usedLetters").style.display = "block";
-    usedLetters = [];
-    document.querySelector(".usedLetters").innerHTML = "";
-    document.querySelector(".container-keyboard").style.display = "block"
-    document.querySelector(".container-game").style.display = "block"
-    document.getElementById("imagen").src=`./images/img00.png`
-    countFail = 0;
-    countHit = 0;
-    document.querySelector(".underscore").innerHTML = "";
-    const countWords = words.length;
-
-    const randomValue = Math.floor(Math.random() * countWords);
-    randomWord = words[randomValue];
-    underscore = randomWord.replace(/./g, "_ ");
-    document.querySelector(".underscore").innerHTML = underscore;   
-}
-
-function gameOver(){
-    // alert("Estás seguro?");
-    countFail = 0;
-    countHit = 0;  
-    btn.disabled = false
-    document.querySelector(".container-keyboard").style.display = "none";
-    document.querySelector(".container-game").style.display = "none";
-
-}
-for(let i = 0; i < btn_letters.length; i++){  
-    btn_letters[i].addEventListener('click', clickOnLetter);
-}
-function endGame(){
-    document.querySelector(".usedLetters").style.display = "none";
-    for(let i = 0; i < btn_letters.length; i++){  
+function endGame() {
+  document.querySelector(".usedLetters").style.visibility = "hidden";
+  document.querySelector(".underscore").style.visibility = "hidden";
+  gameOverBtn = document.querySelector(".gameOver").disabled = true;
+  document.getElementById("imagen").style.visibility = "hidden";
+  btn.disabled = false;
+  document.querySelector(".container-keyboard").style.visibility = "hidden";
+    for (let i = 0; i < btn_letters.length; i++) {
         btn_letters[i].disabled = false;
-    }
-    btn.disabled = false
-    document.querySelector(".container-keyboard").style.display = "none"
+  }
 }
+
+function gameOver() {
+  alert("Are you sure?");
+  gameOverBtn = document.querySelector(".gameOver").disabled = true;
+  document.querySelector(".addWord-container").style.visibility = "visible";
+  document.getElementById("imagen").style.visibility = "hidden";
+  document.querySelector(".underscore").style.visibility = "hidden";
+  for (let i = 0; i < btn_letters.length; i++) {
+    btn_letters[i].disabled = false;
+  }
+  countFail = 0;
+  countHit = 0;
+  btn.disabled = false;
+  document.querySelector(".container-keyboard").style.visibility = "hidden";
+  document.querySelector(".container-game").style.visibility = "hidden";
+  document.querySelector(".usedLetters").style.visibility = "hidden";
+}
+for (let i = 0; i < btn_letters.length; i++) {
+  btn_letters[i].addEventListener("click", clickOnLetter);
+}
+
